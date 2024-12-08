55f5279555e7a3afdf9b42b2da009d68f3f1b285
OndrejVojik <vojik.cz@gmail.com>
2024-12-08 21:40:02 +0100

    Set up Light & Dark Theme, Shadcn, Navbar

---
+++ components/SomeComponent.tsx
@@ -0,0 +1,13 @@
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import React from "react";
+
+const SomeComponent: React.FC = () => {
+  return (
+    <div>
+      <MoonIcon />
+      <SunIcon />
+    </div>
+  );
+};
+
+export default SomeComponent;
