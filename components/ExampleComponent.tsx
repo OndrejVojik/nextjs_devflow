1142f33fbf3638f6c0a0d0d05929dc5f12646cba
OndrejVojik <vojik.cz@gmail.com>
2025-11-20 19:20:48 +0100

    Fix Deployment Issue

--- components/ExampleComponent.tsx
+++ components/ExampleComponent.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const ExampleComponent: React.FC = () => {
   return (
-    <div className="w-12 h-12 bg-lightblue">
-    <div style={{ width: 50, height: 50, backgroundColor: 'lightblue' }}>
-      This div has a width of 50 pixels.
-    </div>
-  );
-};
-
-export default ExampleComponent;
-      This div has a width of 50 pixels.
+    <div className="size-12 bg-blue-400">
+      <div style={{ width: 50, height: 50, backgroundColor: "lightblue" }}>
+        This div has a width of 50 pixels.
+      </div>
     </div>
   );
 };
